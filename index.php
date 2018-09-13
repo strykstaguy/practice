a82e54efd6f47997564bc6669aeaaf7a157e143e
Cody Benner <strykstaguy@gmail.com>
2018-09-12 20:16:22 -0500

    Fixed namespace issue

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 
-//use App\Book;
+use App\Book;
 use App\Customer;
-
+use App\Customer\Basic;
 
 spl_autoload_register(function($className) {
-    $lastSlash = strpos($className, '\\') + 1;
-    $className = substr($className, $lastSlash);
-    $directory = str_replace('\\', '/', $className);
-    $filename = __DIR__ . '/' . $directory . '.php';
-    var_dump($filename); // to see which file is loaded
-    require_once $filename;
+
+    $className = str_replace("\\", DIRECTORY_SEPARATOR, $className);
+    $filename = $_SERVER['DOCUMENT_ROOT'] . '/' . $className . '.php';
+    //var_dump($filename);
+    include_once $filename;
+
 });
 
 function checkIfValid(Customer $customer, array $books): bool
@@ -18,11 +18,11 @@ function checkIfValid(Customer $customer, array $books): bool
     return $customer->getAmountToBorrow() >= count($books);
 }
 
-$book1 = new App\Book(1, "Red Rising", "Pierce Brown", 12);
-$book2 = new App\Book(2, "Morning Star", "Pierce Brown", 1);
+$book1 = new Book(1, "Red Rising", "Pierce Brown", 12);
+$book2 = new Book(2, "Morning Star", "Pierce Brown", 1);
 
-//$customer1 = new Basic(5, 'John', 'Doe', 'johndoe@mail.com');
-//var_dump(checkIfValid($customer1, [$book1])); // ok
+$customer1 = new Basic(5, 'John', 'Doe', 'johndoe@mail.com');
+var_dump(checkIfValid($customer1, [$book1])); // ok
 
 //$customer2 = new Customer(7, 'James', 'Bond', 'james@bond.com');
 //var_dump(checkIfValid($customer2, [$book1])); // fails
